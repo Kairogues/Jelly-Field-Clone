5c3af33755794f330319fa57a9456104e84c6916
Huy Pham <216805013+Kairogues@users.noreply.github.com>
2026-09-17 17:21:23 +0700

    Initial commit

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private GameVisual gameVisual;
+
+
+
+    private void Update()
+    {
+        
+    }
+
+
+    private void HandleInput()
+    {
+        
+    }
+
+
+    private void ProcessGame()
+    {
+        
+    }
+}
