ec1c6307f9f9c3ed704696aa52363d65ef8d6715
Huy Pham <216805013+Kairogues@users.noreply.github.com>
2026-09-20 16:58:25 +0700

    Make CellData and CellDataGrid use Grid2D

--- Assets/Scripts/CellData.cs
+++ Assets/Scripts/CellData.cs
@@ -1,8 +1,42 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+// A grid of 2x2 TileType
 [System.Serializable]
-public struct CellData
+public class CellData
 {
-    public TileType topLeft;
-    public TileType topRight;
-    public TileType botLeft;
-    public TileType botRight;
+    public static int CELL_SIZE = 2;
+    [SerializeField] private Grid2D<TileType> innerTile = new Grid2D<TileType>(new(CELL_SIZE, CELL_SIZE));
+
+    public TileType this[int x, int y]
+    {
+        get => innerTile[x][y];
+        set => innerTile[x][y] = value;
+    }
+
+    public Grid2D<TileType> InnerTile
+    {
+        get => innerTile;
+        set => innerTile = value;
+    }
+
+    public CellData(
+            TileType leftBot = TileType.EMPTY,
+            TileType rightBot = TileType.EMPTY,
+            TileType leftTop = TileType.EMPTY,
+            TileType rightTop = TileType.EMPTY)
+    {
+        innerTile = new Grid2D<TileType>(new(CELL_SIZE, CELL_SIZE));
+
+        innerTile[0][0] = leftBot;
+        innerTile[1][0] = rightBot;
+        innerTile[0][1] = leftTop;
+        innerTile[1][1] = rightTop;
+    }
+
+
+    public void Resize()
+    {
+        innerTile = new Grid2D<TileType>(new(CELL_SIZE, CELL_SIZE));
+    }
 }
--- Assets/Scripts/CellDataGrid.cs
+++ Assets/Scripts/CellDataGrid.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections.Generic;
+using Unity.Mathematics;
 
 [System.Serializable]
 public struct CellDataColumn
@@ -10,59 +11,71 @@ public struct CellDataColumn
 [CreateAssetMenu(fileName = "CellDataGrid", menuName = "CellDataGrid")]
 public class CellDataGrid : ScriptableObject
 {
-    [SerializeField] public int width;
-    [SerializeField] public int height;
-    [SerializeField] public List<CellDataColumn> cellDataGrid;
 
+    [Range(1, 20)]
+    [SerializeField] public int width;
+    [Range(1, 20)]
+    [SerializeField] public int height;
+    [SerializeField] public Grid2D<CellData> cellDataGrid;
+
+
+
+    public CellData this[int x, int y]
+    {
+        get => cellDataGrid[x, y];
+        set => cellDataGrid[x, y] = value;
+    }
+    public int2 Size => new(width, height);
 
 
     private void OnValidate()
     {
-        if (width < 0)
-        {
-            width = 0;
-        }
+        int2 targetSize = new int2(width, height);
 
-        if (height < 0) 
-        {
-            height = 0;
-        }
-
+        // Make sure cellDataGrid exists
         if (cellDataGrid == null)
         {
-            cellDataGrid = new List<CellDataColumn>();
+            cellDataGrid = new Grid2D<CellData>(targetSize);
+        }
+        // Make sure cellDataGrid has correct width
+        if (cellDataGrid.SizeX != width || cellDataGrid.SizeY != height)
+        {
+            cellDataGrid.Resize(targetSize);
+        }
+        // Make sure cellDataGrid has correct height
+        for (int x = 0; x < width; x++)
+        {
+            if (cellDataGrid[x].column.Length != height)
+            {
+                cellDataGrid.Resize(targetSize);
+            }
         }
 
-        while (cellDataGrid.Count < width)
+        for (int x = 0; x < width; x++)
         {
-            cellDataGrid.Add(new CellDataColumn { column = new List<CellData>() });
-        }
-
-        while (cellDataGrid.Count > width)
-        {
-            cellDataGrid.RemoveAt(cellDataGrid.Count - 1);
-        }
-
-        for (int x = 0; x < cellDataGrid.Count; x++)
-        {
-            CellDataColumn column = cellDataGrid[x];
-            
-            if (column.column == null)
+            for (int y = 0; y < height; y++)
             {
-                column.column = new List<CellData>();
-            }
+                // Make sure all CellData in cellDataGrid exists
+                if (cellDataGrid[x][y] == null)
+                {
+                    cellDataGrid[x][y] = new CellData();
+                }
 
-            while (column.column.Count < height)
-            {
-                column.column.Add(new CellData());
-            }
-            
-            while (column.column.Count > height)
-            {
-                column.column.RemoveAt(column.column.Count - 1);
-            }
+                // Make sure all CellData in cellDataGrid has correct size on the X
+                if (cellDataGrid[x][y].InnerTile.SizeX != CellData.CELL_SIZE)
+                {
+                    cellDataGrid[x][y].Resize();
+                }
 
-            cellDataGrid[x] = column;
+                // Make sure all CellData in cellDataGrid has correct size on the Y
+                for (int i = 0; i < CellData.CELL_SIZE; i++)
+                {
+                    if (cellDataGrid[x][y].InnerTile[i].column.Length != CellData.CELL_SIZE)
+                    {
+                        cellDataGrid[x][y].Resize();
+                    }
+                }
+            }
         }
     }
 }
